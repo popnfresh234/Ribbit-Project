7e3f5af7ca6ed9853cf688118569f24e5bbf9ca1
popnfresh234 <pop_n_fresh234@hotmail.com>
2014-06-29 11:51:55 +0800

    Relating Users

---
+++ src/com/teamtreehouse/ribbit/ParseConstants.java
@@ -0,0 +1,10 @@
+package com.teamtreehouse.ribbit;
+
+public final class ParseConstants {
+	//Class Name
+	
+	//Field names
+	public static final String KEY_USERNAME = "username";
+	public static final String KEY_FRIENDS_RELATION = "FriendsRelation";
+	
+}
---
+++ src/com/teamtreehouse/ribbit/RibbitApplication.java
@@ -0,0 +1,14 @@
+package com.teamtreehouse.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class RibbitApplication extends Application {
+	
+	@Override
+	public void onCreate() { 
+		super.onCreate();
+		Parse.initialize(this, "XK0x5U5x1QCmfTbuT8wSxHDCSwie66bDp9L4ZW5Q", "Ced2wQOmFuqlJiA4HLaIcgbB4f72XhqazK2MhamD");
+	}
+}
